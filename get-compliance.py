c930d9fb36776e24a4bd1ae0382f3ed39a03e743
Arush Khare <arush.ashish.khare@gmail.com>
2025-10-18 14:41:44 -0700

    au

--- get-compliance.py
+++ get-compliance.py
@@ -1,4 +1,4 @@
-from browser_use import Agent, ChatGoogle, BrowserProfile
+from browser_use import Agent, ChatGoogle, BrowserProfile, BrowserSession
 from dotenv import load_dotenv
 import asyncio
 import os
@@ -8,11 +8,18 @@ load_dotenv()
 PROFILE_DIR = os.path.expanduser("~/my_browser_profile")
 profile = BrowserProfile(path=PROFILE_DIR, headless=True)
 
+task = \
+"""
+Find compliance laws for the European Union (EU) regarding data privacy.
+Read through the latest document (not all documents) released by the EU and retrieve key policies.
+"""
+
 async def main():
     llm = ChatGoogle(model="gemini-flash-latest")
     task = "Find compliance laws for the European Union regarding data privacy."
     agent = Agent(task=task, llm=llm, profile=profile)
     await agent.run()
+    print("Agent task completed. Results saved to eu_compliance_results.txt")
 
 if __name__ == "__main__":
     asyncio.run(main())
